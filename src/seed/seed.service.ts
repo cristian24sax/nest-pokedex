7948e3ab4bdcd6fa2dacc0b95dc724c1af740b8a
cristian24sax <cristian24jq@gmail.com>
2023-05-04 20:33:34 -0500

    add change before lote

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokemonResponse } from './interfaces/poke.interface';
+
+@Injectable()
+export class SeedService {
+  private readonly axios: AxiosInstance = axios;
+
+  async executeSeed() {
+    const { data } = await this.axios.get<PokemonResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=1',
+    );
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+      console.log({name,no})
+    });
+    return data.results;
+  }
+}
