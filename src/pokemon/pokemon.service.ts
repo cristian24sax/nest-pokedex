c8ef4efce3c3172c1229ec6d126946b0d22f8830
cristian24sax <cristian24jq@gmail.com>
2023-05-05 00:53:14 -0500

    findAll() and limit, offset

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -3,12 +3,14 @@ import {
   Injectable,
   InternalServerErrorException,
   NotFoundException,
+  Query,
 } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Model, isValidObjectId } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class PokemonService {
@@ -31,8 +33,15 @@ export class PokemonService {
   async inserSeed(createPokemonDto: CreatePokemonDto[]) {
     return this.pokemonModel.insertMany(createPokemonDto);
   }
-  findAll() {
-    return this.pokemonModel.find()
+
+  findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
+    return this.pokemonModel
+      .find()
+      .limit(limit)
+      .skip(offset)
+      .sort({ no: 1 })
+      .select('-__v');
   }
 
   async findOne(term: string) {
